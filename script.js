573be6914e437d2369df3440c00c18a3f141b58a
DelimaAnton <champika.delima@gmail.com>
2023-10-26 22:01:26 +0530

    a

---
+++ script.js
@@ -0,0 +1,98 @@
+
+//smart watch scroll
+
+document.getElementById('smartWatchDD').addEventListener('click', function() {
+    
+    var smartWatchPg = 'index2.html';
+    
+   
+    var targetElementId = 'sw';
+
+    
+    var fullURL = smartWatchPg + '#' + targetElementId;
+
+    
+    window.location.href = fullURL;
+  });
+
+
+//smart phone scroll
+
+  document.getElementById('spDD').addEventListener('click', function() {
+    
+    var sppage = 'index.html';
+    
+   
+    var targetElementId = 'sp';
+
+    
+    var fullURL = sppage + '#' + targetElementId;
+
+    
+    window.location.href = fullURL;
+  });
+
+  //laptops scroll
+
+  document.getElementById('laptopsDD').addEventListener('click', function() {
+    
+    var laptopsPage = 'index.html';
+    
+   
+    var targetElementId = 'laptops';
+
+    
+    var fullURL = laptopsPage + '#' + targetElementId;
+
+    
+    window.location.href = fullURL;
+  });
+
+  //gaming consoles scroll
+
+  document.getElementById('gcDD').addEventListener('click', function() {
+    
+    var gcPage = 'index2.html';
+    
+   
+    var targetElementId = 'gamingConsoles';
+
+    
+    var fullURL = gcPage + '#' + targetElementId;
+
+    
+    window.location.href = fullURL;
+  });
+
+  //drones scroll
+
+  document.getElementById('dronesDD').addEventListener('click', function() {
+    
+    var dronesPage = 'index2.html';
+    
+   
+    var targetElementId = 'drones';
+
+    
+    var fullURL = dronesPage + '#' + targetElementId;
+
+    
+    window.location.href = fullURL;
+  });
+
+  //cameras scroll
+
+  document.getElementById('camerasDD').addEventListener('click', function() {
+    
+    var camerasPage = 'index2.html';
+    
+   
+    var targetElementId = 'cameras';
+
+    
+    var fullURL = camerasPage + '#' + targetElementId;
+
+    
+    window.location.href = fullURL;
+  });
+ 
